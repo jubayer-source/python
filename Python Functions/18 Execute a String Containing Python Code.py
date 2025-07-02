058361c094d16b2d86dfa5dff71f937ca99889c9
Md. Jubayer Ahmad <mdjubayerahmad16@gmail.com>
2025-07-02 20:40:10 +0600

    Create 18 Execute a String Containing Python Code.py

---
+++ Python Functions/18 Execute a String Containing Python Code.py
@@ -0,0 +1,16 @@
+# jubayer-source github
+# jubayer_source username list
+
+mycode = 'print("hello world")'
+exec(mycode) # exec() function to execute dynamically defined code stored as strings
+
+
+
+code = """
+def multiply(x,y):
+    return x * y
+
+print('Multiply of 2 and 3 is: ', multiply(2,3))
+"""
+
+exec(code)
