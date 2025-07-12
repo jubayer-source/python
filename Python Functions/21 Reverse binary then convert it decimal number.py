16ebe0b7e48feee7a6ca93cde6ba7e1d259829e8
Md. Jubayer Ahmad <mdjubayerahmad16@gmail.com>
2025-07-13 01:26:53 +0600

    Create 21 Reverse binary then convert it decimal number.py

---
+++ Python Functions/21 Reverse binary then convert it decimal number.py
@@ -0,0 +1,17 @@
+n = 10001010101
+
+rev_n = str(n)[::-1]
+
+rev_int_num = int(rev_n,2)
+
+print(rev_int_num)
+
+#######################
+n = "10101010010101001010"
+
+rev = n[::-1]
+
+reversed_decimal = int(rev, 2)
+
+print("Reversed binary string:", rev)
+print("As decimal number:", reversed_decimal)
