2c9199348ebef2eec46f91f02ebcf031d8533a73
Md. Jubayer Ahmad <mdjubayerahmad16@gmail.com>
2025-07-23 13:14:39 +0600

    Create Nested List, Filtering.py

---
+++ python list/Nested List, Filtering.py
@@ -0,0 +1,12 @@
+matrix = [[1, 2], [3, 4]]
+print(matrix[1][0])  # Second row (index 1) er first element = 3
+
+flat = [item for row in matrix for item in row]
+print(flat)              # Nested list ke flat kore: [1, 2, 3, 4]
+
+even_numbers = [x for x in range(10) if x % 2 == 0]
+print(even_numbers)      # 0 theke 9 porjonto shob even number
+
+nums = [3, 7, 1, 5]
+print(sum(nums))
+print(sorted(nums))
