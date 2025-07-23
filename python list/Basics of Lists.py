4a6c326e22db52511f2ddbe3511f390a6afaefb9
Md. Jubayer Ahmad <mdjubayerahmad16@gmail.com>
2025-07-23 13:01:34 +0600

    Create Basics of Lists.py

---
+++ python list/Basics of Lists.py
@@ -0,0 +1,17 @@
+# Create a list
+fruits = ["apple", "banana", "cherry"]
+
+# Access elements
+print(fruits[0])  # "apple"
+
+# Modify elements
+fruits[1] = "blueberry"
+
+# Add elements
+fruits.append("date")
+
+# Remove elements
+fruits.remove("cherry")
+
+# Length of list
+print(len(fruits))  # 3
